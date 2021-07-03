983946cf8f4135f22140a355ab23d3a61789fa1f
nitin dutta <nitin@techtonic.asia>
2021-07-03 16:55:32 +0530

    Bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        ></meta>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
