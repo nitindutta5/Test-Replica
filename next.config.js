b45c89ea1876b0b09a92e065ca496cf312b8c93e
nitin dutta <nitin@techtonic.asia>
2021-07-05 11:13:16 +0530

    Testing updated version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   distDir: 'out',
-  target:'serverless',
-  images: {
-    domains: ['https://jsll-lifestyle.netlify.app/'],
-  },
+  target:'serverless'
 }
