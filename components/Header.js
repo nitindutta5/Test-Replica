f4cc598f41c21c0bfffb3531f3b12cc1a39fe0a4
nitin dutta <nitin@techtonic.asia>
2021-07-08 16:48:00 +0530

    Navigation halfway through

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,6 @@
 import React, { useState, useContext } from 'react';
 import {
-    Collapse,
     Navbar,
-    NavbarToggler,
     NavbarBrand,
     Nav,
     Button
@@ -25,12 +23,25 @@ const Header = () => {
                 <NavbarBrand href="/"> <img src={dark ? logo2 : logo} alt="logo" className="img-fluid " /></NavbarBrand>
                 <Nav className="ms-auto align-items-center" navbar>
                     <Button color="primary" className="me-lg-5 me-4">
-                        <img src="./Download-Brochure.svg" /> Brochure
+                        <img src="./Download-Brochure_02.svg" /> Brochure
                     </Button>
-                    <img src={dark ? hamburger2 : hamburger} alt="hamburger" className="img-fluid hamburger" />
+                    <img onClick={toggle} src={dark ? hamburger2 : hamburger} alt="hamburger" className="img-fluid hamburger" />
                 </Nav>
-
             </Navbar>
+            <div id="myNav" className={isOpen?"overlay open":"overlay"} >
+
+
+  <a  className="closebtn" onClick={toggle}>&times;</a>
+
+  <div className="overlay-content">
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+  </div>
+
+</div>
+
         </div>
     );
 }
--- components/NextBtn.js
+++ components/NextBtn.js
@@ -1,7 +1,7 @@
 const NextBtn = (props) => {
     const { onClick, className, style } = props;
     return (
-		<svg id="next" style={style} className={className} onClick={onClick} xmlns="http://www.w3.org/2000/svg" width="75.263" height="75.263" viewBox="0 0 75.263 75.263"><g transform="translate(75.263 75.263) rotate(180)"><ellipse cx="37.631" cy="37.632" rx="37.631" ry="37.632" fill="#fff"/><path d="M12232.827,4756l-13.487,13.487,13.487,13.488" transform="translate(-12188.588 -4731.892)" fill="none" stroke="#828692" stroke-linecap="round" stroke-width="2"/></g></svg>
+		<svg id="next" style={style} className={className} onClick={onClick} xmlns="http://www.w3.org/2000/svg" width="75.263" height="75.263" viewBox="0 0 75.263 75.263"><g transform="translate(75.263 75.263) rotate(180)"><ellipse cx="37.631" cy="37.632" rx="37.631" ry="37.632" fill="#fff"/><path d="M12232.827,4756l-13.487,13.487,13.487,13.488" transform="translate(-12188.588 -4731.892)" fill="none" stroke="#828692" strokeLinecap="round" strokeWidth="2"/></g></svg>
     )
 }
 
